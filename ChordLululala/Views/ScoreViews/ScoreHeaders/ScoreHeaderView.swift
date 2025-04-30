8dc0f0a21f08eb17726958976e2dfb0803d8fab7
minjun kim <minzzzun@gmail.com>
2025-04-30 16:28:15 +0900

    [Feat] : 헤더바 UI에 설정 버튼 추가

--- ChordLululala/Views/ScoreViews/ScoreHeaders/ScoreHeaderView.swift
+++ ChordLululala/Views/ScoreViews/ScoreHeaders/ScoreHeaderView.swift
@@ -11,93 +11,111 @@ import SwiftUI
 struct ScoreHeaderView: View {
     @ObservedObject var viewModel : ScoreHeaderViewModel
     @EnvironmentObject var router : NavigationRouter
-    
+
+
     
     var body: some View {
-        HStack(spacing:0){
-            /// 뒤로가기
-            Button(action:{
-                router.back()
-            }){
-                Image("scoreheader_back")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 36, height: 36)
-                    .foregroundColor(Color.primaryBaseBlack)
-            }
+        GeometryReader { geo in
+            let isLandscape = geo.size.width > geo.size.height // 화면이 가로모드이면 true, 세로모드이면 false
+            let leftSpacerWidth: CGFloat = isLandscape ? 456 : 16
             
-            Spacer()
-            
-            /// 제목
-            Text(viewModel.truncatedTitle)
-                .foregroundColor(Color.primaryGray900)
-                .textStyle(.headingLgSemiBold)
-                .lineLimit(1)                    // 한 줄로 제한
-                .truncationMode(.tail)          // 넘치면 뒤에 “…” 표시
-                .layoutPriority(1)
-            Spacer()
-            
-            HStack(spacing: 7){
-                /// 펜슬
+            HStack(spacing:0){
+                /// 뒤로가기
                 Button(action:{
-                    print("펜슬 기능 클릭") // TODO: 기능 추가해야함
+                    router.back()
                 }){
-                    Image("scoreheader_pencil")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 36, height: 36)
-                        .foregroundColor(Color.primaryBaseBlack)
-                }
-                .padding(.trailing,10)
-                
-                /// 페이지 추가버튼
-                Button(action:{
-                    print("페이지 추가버튼 클릭") //TODO: 기능 추가해야함
-                }){
-                    Image("scoreheader_page_add")
+                    Image("scoreheader_back")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 36, height: 36)
                         .foregroundColor(Color.primaryBaseBlack)
                 }
                 
-                /// 키변환
-                Button(action:{
-                    print("키변환 기능 클릭") //TODO: 기능 추가해야함
-                }){
-                    HStack{
-                        Image("scoreheader_chordchange")
+                Spacer().frame(width: leftSpacerWidth)
+                
+                /// 제목
+                Text(viewModel.truncatedTitle)
+                    .foregroundColor(Color.primaryGray900)
+                    .textStyle(.headingLgSemiBold)
+                    .layoutPriority(1)
+                    .lineLimit(1)
+                Spacer()
+                
+                HStack(spacing: 7){
+                    /// 펜슬
+                    Button(action:{
+                        print("펜슬 기능 클릭") // TODO: 기능 추가해야함
+                    }){
+                        Image("scoreheader_pencil")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 24, height: 24)
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(Color.primaryBaseBlack)
+                    }
+                    .padding(.trailing,10)
+                    
+                    /// 페이지 추가버튼
+                    Button(action:{
+                        print("페이지 추가버튼 클릭") //TODO: 기능 추가해야함
+                    }){
+                        Image("scoreheader_page_add")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(Color.primaryBaseBlack)
+                    }
+                    
+                    /// 키변환
+                    Button(action:{
+                        print("키변환 기능 클릭") //TODO: 기능 추가해야함
+                    }){
+                        HStack{
+                            Image("scoreheader_chordchange")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                            
+                            Text("키변환")
+                                .textStyle(.headingLgMedium)
+                        }
+                    }
+                    .frame(width: 94, height: 42)
+                    .background(Color.primaryBlue500)
+                    .foregroundColor(Color.primaryBaseWhite)
+                    .cornerRadius(8)
+                    .padding(.trailing,10)
+                    
+                    ///전체 페이지 보기
+                    Button(action:{
                         
-                        Text("키변환")
-                            .textStyle(.headingLgMedium)
+                        print("전체 페이지 보기 클릭") //TODO: 기능 추가해야함
+                    }){
+                        Image("scoreheader_page_list")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(Color.primaryBaseBlack)
+                        
+                    }
+                    
+                    ///설정
+                    Button(action:{
+                        
+                        print("전체 페이지 보기 클릭") //TODO: 기능 추가해야함
+                    }){
+                        Image("scoreheader_setting")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(Color.primaryBaseBlack)
+                        
                     }
                 }
-                .frame(width: 94, height: 42)
-                .background(Color.primaryBlue500)
-                .foregroundColor(Color.primaryBaseWhite)
-                .cornerRadius(8)
-                .padding(.trailing,10)
                 
-                ///전체 페이지 보기
-                Button(action:{
-                    
-                    print("전체 페이지 보기 클릭") //TODO: 기능 추가해야함
-                }){
-                    Image("scoreheader_page_list")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 36, height: 36)
-                        .foregroundColor(Color.primaryBaseBlack)
-                    
-                }
             }
-            
+            .padding(.leading, 22)
+            .padding(.trailing, 22)
         }
-        .padding(.leading, 22)
-        .padding(.trailing, 22)
         .frame(height: 83)
         
     }
