68ee754a8620608a6133fad8f4a5b0c94de0534d
minjun kim <minzzzun@gmail.com>
2025-05-05 14:46:46 +0900

    [Feat] : 키변환 클릭시 로딩 UI 구현

--- ChordLululala/Views/ScoreViews/ScoreView.swift
+++ ChordLululala/Views/ScoreViews/ScoreView.swift
@@ -18,19 +18,21 @@ struct ScoreView : View {
             /// 악보 헤더부분
             if !viewModel.playmodeViewModel.isOn {
                 ScoreHeaderView(viewModel: viewModel.headerViewModel,
-                                annotationVM: viewModel.annotationViewModel
+                                annotationVM: viewModel.annotationViewModel,
+                                isTransposing: viewModel.isTransposingViewModel
                 )
-                    .transition(.move(edge: .top).combined(with: .opacity)) // 슬라이드 인아웃 + 페이드 효과
+                .transition(.move(edge: .top).combined(with: .opacity)) // 슬라이드 인아웃 + 페이드 효과
             }
-                
+            
             /// 악보 바디 뷰
             ScoreMainBodyView(
                 pdfViewModel: viewModel.pdfViewModel,
                 playmodeViewModel: viewModel.playmodeViewModel,
                 pageNavViewModel: viewModel.pageNavViewModel,
-                annotationVM: viewModel.annotationViewModel
+                annotationVM: viewModel.annotationViewModel,
+                isTransposing: viewModel.isTransposingViewModel
             )
-        
+            
         }
         .navigationBarHidden(true)
         
